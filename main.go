9fc7c84bc09e1ed57211f8ffccbf93ecf6019592
mrfleming <ericfleming8@gmail.com>
2021-05-12 18:59:38 -0500

    refactor: broke out splitting into its own func

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"strings"
 
@@ -16,44 +15,50 @@ type ChecklistItem struct {
 
 func main() {
 
+	// grab user arguments
 	inputFilename := os.Args[1]
 	outputFilename := os.Args[2]
 
+	// open file
 	file, err := os.OpenFile(inputFilename, os.O_RDWR|os.O_CREATE, os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
+	// Read into collection
 	checklistItems := []*ChecklistItem{}
-
 	if err := gocsv.UnmarshalFile(file, &checklistItems); err != nil { // Load clients from file
 		panic(err)
 	}
-	for _, item := range checklistItems {
-		fmt.Println(item)
-	}
-	fmt.Println(checklistItems[0])
-	fmt.Println("-------------------------------------")
 
+	//build the new list
+	var newCSVList []ChecklistItem
+	newCSVList = splitData(checklistItems)
+
+	// print to new file
+	outfile, err := os.Create(outputFilename)
+	gocsv.MarshalFile(&newCSVList, outfile)
+}
+
+func splitData(items []*ChecklistItem) []ChecklistItem {
 	// New list to print a new csv file
 	var newCSVList []ChecklistItem
 
 	//build new list
-	for i := 0; i < len(checklistItems); i++ {
-		formArray := strings.Split(checklistItems[i].Form, "\n")
+	for i := 0; i < len(items); i++ {
+		formArray := strings.Split(items[i].Form, "\n")
 		formSize := len(formArray)
 
 		for f := 0; f < formSize; f++ {
 			newCSVList = append(newCSVList, ChecklistItem{
-				Category:  checklistItems[i].Category,
-				FormGroup: checklistItems[i].FormGroup,
+				Category:  items[i].Category,
+				FormGroup: items[i].FormGroup,
 				Form:      formArray[f],
 			})
 		}
 
 	}
 
-	outfile, err := os.Create(outputFilename)
-	gocsv.MarshalFile(&newCSVList, outfile)
+	return newCSVList
 }
